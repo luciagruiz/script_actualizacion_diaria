f3412bb31ed870412b444be03ad76af4da190c4e
fjhuete <fjhuete.m@gmail.com>
2024-05-22 13:06:52 +0200

    Modificaciones menores de estilo. Ayuda completa.

--- actualizacion.sh
+++ actualizacion.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 #Autor: Lucía Garrote Ruiz, Francisco Javier Huete Mejías
-#Descripción:
-#Versión: 1.0
+#Descripción: Este script actualiza el sistema diariamente. Se debe ejecutar con privilegios de root.
+#Versión: 1.28
 #Fecha:
 #Zona de depuración
         #Inicio de la zona de depuración con set -x (descomentar para activar)
@@ -13,25 +13,9 @@
 #Color de texto
 rojo="\e[1;31m"
 verde="\e[1;32m"
-amarillo="\e[1;33m"
-azul="\e[1;34m"
-morado="\e[1;35m"
-cyan="\e[1;36m"
-
-#Color de fondo
-gris="\e[1;40m"
-amarillo="\e[1;44m"
-azul="\e[1;44m"
-morado="\e[1;45m"
-cyan="\e[1;46m"
 
 #Formato
 negrita="\e[1m"
-subrayado="\e[4m"
-parpadeo="\e[1;5m"
-invertido="\e[1;7m"
-
-
 fin_formato="\e[0m"
 
 #Zona de declaración de funciones
@@ -60,7 +44,7 @@ Para configurar una actualización diaria del sistema a las 9 de la mañana:
 }
 
 mostrar_version() {
-echo "$0 Versión: 1.0"
+echo "$0 Versión: 1.28"
 exit 0
 }
 
