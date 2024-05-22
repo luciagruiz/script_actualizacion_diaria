6579bbdc5f47edbcd253d9075c491b367202b1ca
luu <luciagruiz9@gmail.com>
2024-05-22 10:12:26 +0200

    Corregida ruta de programación del script + Agregada función programar_ejecucion_basica

--- actualizacion.sh
+++ actualizacion.sh
@@ -38,22 +38,18 @@ fin_formato="\e[0m"
 
 mostrar_ayuda() {
 	echo -e ""$negrita"Uso:"$fin_formato" $0 [OPCIÓN]
-"$negrita"Descripción:"$fin_formato" Este script actualiza el sistema \
-diariamente. Se debe ejecutar con privilegios de root.
+"$negrita"Descripción:"$fin_formato" Este script actualiza el sistema diariamente. Se debe ejecutar con privilegios de root.
 "$negrita"Parámetros aceptados:"$fin_formato"
-	-a Muestra la hora a la que se ha programado la ejecución diaria de \
-este script
+	-a Muestra la hora a la que se ha programado la ejecución diaria de este script
 	-c Configura el script de forma interactiva
-	-p HORA Programa la ejecución diaria del script a una hora determinada. \
-El formato aceptado para la hora es HH:MM con una hora entre 00 y 24.
+	-p HORA Programa la ejecución diaria del script a una hora determinada. El formato aceptado para la hora es HH:MM con una hora entre 00 y 24.
 	-h Muestra esta ayuda
 	-v Muestra la versión 
 "$negrita"Ejemplos de uso: "$fin_formato"
 Para actualizar el sistema:
 	$0
 		
-Para comprobar la hora a la que hay programada una actualización \
-diaria del sistema:
+Para comprobar la hora a la que hay programada una actualización diaria del sistema:
 	$0 -a
 		
 Para configurar una actualización diaria del sistema de forma interactiva:
@@ -83,8 +79,7 @@ validar_root() {
 	if [ $(whoami) = 'root' ]; then
 		return 0
 	else
-		echo -e "$rojo$negrita[ERROR]$fin_formato - Este script se debe \
-ejecutar como root."
+		echo -e "$rojo$negrita[ERROR]$fin_formato - Este script se debe ejecutar como root."
 		exit 1
 	fi
 }
@@ -122,8 +117,7 @@ actualizar_repo() {
     [[ $distro = "tearch" ]] || command -v pacman &> /dev/null; then
         pacman -Syu --noconfirm >> "/var/log/auto_update.log" 2>&1
     else
-        echo -e "$rojo$negrita[ERROR]$fin_formato -  No se ha podido \
-determinar el gestor de paquetes del sistema"
+        echo -e "$rojo$negrita[ERROR]$fin_formato -  No se ha podido determinar el gestor de paquetes del sistema"
         exit 1
     fi
 }
@@ -132,8 +126,7 @@ determinar el gestor de paquetes del sistema"
 pregunta() {
     read -p "¿Desea que el sistema se actualice diariamente? (s/n): " respuesta
     if [ "$respuesta" = "s" ]; then
-        read -p "Por favor, ingrese la hora de actualización (formato HH:MM, \
-por ejemplo, 09:00): " hora
+        read -p "Por favor, ingrese la hora de actualización (formato HH:MM, por ejemplo, 09:00): " hora
         programar_ejecucion
     else
         echo -e "$verde$negrita[OK]$fin_formato - No se realizará la actualización diaria."
@@ -143,26 +136,58 @@ por ejemplo, 09:00): " hora
 # Obtener hora de la ejecución diaria ya programada
 comprobar_ejecucion_diaria() {
     # Comprobar si la línea ya está presente en el crontab
-    if grep -q "$PWD/$0" /etc/crontab; then
-        linea_existente=$(grep "$PWD/$0" /etc/crontab)
+    if grep -q "$(realpath $0)" /etc/crontab; then
+        linea_existente=$(grep "$(realpath $0)" /etc/crontab)
         hora_existente=$(echo "$linea_existente" | awk '{print $2}')
         minuto_existente=$(echo "$linea_existente" | awk '{print $1}')
-        echo -e "$verde$negrita[OK]$fin_formato - La actualización diaria está programada para \
-las $hora_existente:$minuto_existente"
+        echo -e "$verde$negrita[OK]$fin_formato - La actualización diaria está programada para las $hora_existente:$minuto_existente"
     else
         echo "No hay ninguna ejecución diaria programada."
     fi
 }
 
-# Programar la ejecución diaria a la hora especificada
+# Programar la ejecución diaria a la hora especificada de forma no interactiva ej: actualizacion.sh -p 08:15 (lo programa a las 08:15)
+programar_ejecucion_basica() {
+    # Verificar si se proporciona un argumento
+    if [ -z "$1" ]; then
+        echo -e "${rojo}${negrita}[ERROR]${fin_formato} - No se proporcionó ninguna hora para programar la ejecución diaria."
+        exit 1
+    fi
+
+    hora="$1"
+    hora2=$(echo "$hora" | sed 's/^0*//' | cut -d':' -f1)
+    minuto=$(echo "$hora" | cut -d':' -f2)
+
+    # Convertir horas y minutos a números enteros
+    hora2=$((10#$hora2))
+    minuto=$((10#$minuto))
+
+    # Verificar si la hora y los minutos están en el rango correcto
+    if (( hora2 < 0 || hora2 > 23 || minuto < 0 || minuto > 59 )); then
+        echo -e "${rojo}${negrita}[ERROR]${fin_formato} - La hora especificada no es válida. Debe estar en formato HH:MM con una hora entre 00 y 23 y minutos entre 00 y 59."
+        exit 1
+    fi
+
+    # Eliminar la ejecución diaria anterior si existe
+    if grep -q "$(realpath $0)" /etc/crontab; then
+        sudo sed -i "\~^.*$(realpath $0).*\$~d" /etc/crontab
+        echo -e "${verde}${negrita}[OK]${fin_formato} - Se eliminó la ejecución diaria anteriormente programada."
+    fi
+
+    # Programar la nueva ejecución diaria
+    echo "$minuto $hora2 * * * root $(realpath $0)" | sudo tee -a /etc/crontab >/dev/null
+    echo -e "${verde}${negrita}[OK]${fin_formato} - Se programará la actualización diaria a las $hora."
+}
+
+
+# Programar la ejecución diaria a la hora especificada de forma interactiva
 programar_ejecucion() {
     # Comprobar si la línea ya está presente en el crontab
-    if grep -q "$PWD/$0" /etc/crontab; then
-        read -p "Ya hay una ejecución diaria del script programada, \
-¿deseas eliminarla? (s/n): " respuesta2
+    if grep -q "$(realpath $0)" /etc/crontab; then
+        read -p "Ya hay una ejecución diaria del script programada, ¿deseas eliminarla? (s/n): " respuesta2
         if [ "$respuesta2" = "s" ]; then
             # Eliminar la línea del crontab
-            sudo sed -i "\~^.*$PWD/$0.*\$~d" /etc/crontab
+            sudo sed -i "\~^.*$(realpath $0).*\$~d" /etc/crontab
             echo -e "$verde$negrita[OK]$fin_formato - Se eliminó la ejecución diaria anteriormente programada."
             
             # Extraer la hora y el minuto de la variable $hora
@@ -171,21 +196,20 @@ programar_ejecucion() {
         
             # Agregar la línea al crontab con la hora especificada por el 
             #usuario
-            echo "$minuto $hora2 * * * root $PWD/$0" | sudo tee -a 
-            /etc/crontab >/dev/null
+            echo "$minuto $hora2 * * * root $(realpath $0)" | sudo tee -a /etc/crontab >/dev/null
             echo -e "$verde$negrita[OK]$fin_formato - Se programará la actualización diaria a las $hora"
         
         else
             comprobar_ejecucion_diaria
         fi
     
-    elif ! grep -q "$PWD/$0" /etc/crontab; then
+    elif ! grep -q "$(realpath $0)" /etc/crontab; then
         # Extraer la hora y el minuto de la variable $hora
         hora2=$(echo "$hora" | cut -d':' -f1)
         minuto=$(echo "$hora" | cut -d':' -f2)
         
         # Agregar la línea al crontab con la hora especificada por el usuario
-        echo "$minuto $hora2 * * * root $PWD/$0" | sudo tee -a /etc/crontab >/dev/null
+        echo "$minuto $hora2 * * * root $(realpath $0)" | sudo tee -a /etc/crontab >/dev/null
         echo -e "$verde$negrita[OK]$fin_formato - Se programará la actualización diaria a las $hora"
     fi
 }
@@ -193,14 +217,15 @@ programar_ejecucion() {
 #Zona del script
 
 #Control de argumentos
-while getopts "achv" opcion; do
-	case $opcion in
-		a) comprobar_ejecucion_diaria; exit 0;;
-		c) pregunta; exit 0;; 
-		h) mostrar_ayuda; exit 0;;
-		v) mostrar_version ;;
-		?) mostrar_ayuda; exit 1 ;;
-	esac
+while getopts "achvp:" opcion; do
+    case $opcion in
+        a) comprobar_ejecucion_diaria; exit 0;;
+        c) pregunta; exit 0;; 
+        p) programar_ejecucion_basica "$OPTARG"; exit 0;;
+        h) mostrar_ayuda; exit 0;;
+        v) mostrar_version ;;
+        ?) mostrar_ayuda; exit 1 ;;
+    esac
 done
 
 validar_root
